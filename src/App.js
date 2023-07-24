b17b9d9cd3bd6fa5ae4be9bdf10456c4be0471e6
alptdogan <alpdogan000@gmail.com>
2023-07-24 18:22:52 +0300

    Commit berfore deleteTodo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { Component } from "react";
+import React from "react";
 import { TodoWrapper } from "./components/TodoWrapper";
 
 function App() {
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
+
+uuidv4();
 
 export const TodoWrapper = () => {
-  return <div>TodoWrapper</div>;
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
+
+  // üstüne çizgi çekmek için. prop olarak aşağıda Todo'ya geçtik.
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo
+          task={todo}
+          key={index}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </div>
+  );
 };
